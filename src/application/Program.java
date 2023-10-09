676606be9aa97474185d8b16d00beb1792424472
yrodr <y.rodr1gues0811@outlook.com>
2023-10-09 14:20:59 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,5 +24,12 @@ public class Program {
         System.out.println("\n=== TEST 3: seller findAll===");
         list = sellerDao.findAll();
         list.forEach(System.out::println);
+
+        System.out.println("\n=== TEST 4: seller insert===");
+        Seller obj = new Seller(null, "Greg", "greg@gmail.com", new Date(),
+                4000.00, department);
+
+        sellerDao.insert(obj);
+        System.out.println("Inserted! new ID = " + obj.getId());
     }
 }
